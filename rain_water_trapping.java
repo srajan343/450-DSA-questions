29756fb93d08c0bbcbfe08080427d736625fba59
Srajan sethiya <77824617+srajan343@users.noreply.github.com>
2023-04-17 15:00:10 +0530

    Add files via upload

---
+++ rain_water_trapping.java
@@ -0,0 +1,25 @@
+public class rain_water_trapping {
+    public static void main(String[] args) {
+        int[] a = {3,1,2,4,0,1,3,2};
+        System.out.println(water(a));
+    }
+
+    static int water(int[] arr){
+        int n = arr.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        for(int i=1;i<n;i++){
+            left[0] = arr[0];
+            left[i] = Math.max(left[i-1], arr[i]); 
+        }
+        right[n-1] = arr[n-1];
+        for(int i = n-2;i>=0;i--){
+            right[i] = Math.max(right[i+1], arr[i]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+= Math.min(left[i],right[i]) - arr[i];
+        }
+        return ans;
+    }
+}
